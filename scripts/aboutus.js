738ab9e755392f6d2e2aadd1e4087a0520209166
KAMAbee <kamil.akbarov.94@gmail.com>
2024-10-28 01:23:27 +0500

    Akbarov pages

--- scripts/aboutus.js
+++ scripts/aboutus.js
@@ -17,3 +17,21 @@ sound_btn.addEventListener('click', () => {
     const sound = document.querySelector('.notification');
     sound.play();
 });
+
+
+//Map
+function init() {
+	let map = new ymaps.Map('map', {
+		center: [51.0905113,71.4186188],
+		zoom: 17
+	});
+
+    map.controls.remove('geolocationControl');
+    map.controls.remove('searchControl');
+    map.controls.remove('fullscreenControl');
+    map.controls.remove('rulerControl');
+    map.controls.remove('typeSelector');
+    map.controls.remove('trafficControl');
+}
+
+ymaps.ready(init);
